d7681123c84f8dd8a8c652360043030fe2d315fa
Alejandra Luna <alunam2@est.ups.edu.ec>
2025-10-27 13:46:43 -0500

    Fix: Error en redondeo de decimales a 2

--- conversor.py
+++ conversor.py
@@ -1,37 +1,49 @@
 import json
 from datetime import datetime
+import random
+
 def cargar_tasas(ruta):
-  """ Lee un archico json y retorna un objeto """
-  with open(ruta, "r") as archivo:
-    return json.load(archivo)
+    """ Lee un archivo json y retorna un objeto """
+    with open(ruta, "r") as archivo:
+        return json.load(archivo)
+
 
 def convertir(precio_usd, moneda_destino, tasas):
-  """ Convierte el valor a otra moneda """
-  #obtiene la tasa de cambio de USD --> moneda_destino
-  tasa = tasas["USD"].get(moneda_destino)
-  
-  if not tasa:
-   raise ValueError("Moneda no soportada")
-  return precio_usd * tasa
+    """ Convierte el valor a otra moneda """
+    # Obtiene la tasa de cambio de USD -> moneda_destino
+    tasa = tasas["USD"].get(moneda_destino)
+    # Si la moneda de destino no existe, lanza una excepcion
+    if not tasa:
+        raise ValueError("Moneda no soportada")
+    precio_convertido = precio_usd * tasa
+    return round(precio_convertido, 2)
+
 
 def registrar_transaccion(producto, precio_convertido, moneda, ruta_log):
- """ Escribe una nueva linea en el archivo de registro"""
- with open(ruta_log, "a") as archivo:
-  # Obtener la fecha actual con formato yyyy-mm-dd HH: mm:ss 
-  fecha = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-  #Escribir una linea nueva en el archivo de registro
-  archivo.write(f"{fecha} | {producto}: {precio_convertido:.2f} {moneda}\n")
+    """ Escribe una nueva linea en el archivo de registro """
+    with open(ruta_log, "a") as archivo:
+        # Obtener la fecha actual con formato yyyy-mm-dd HH:mm:ss
+        fecha = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        # Escribir una línea nueva en el archivo de registro
+        archivo.write(f"{fecha} | {producto}: {precio_convertido:.2f} {moneda}\n")
+
+def actualizar_tasas(ruta):
+    # Simular API: Cambiar tasas aleatoriamente ±2%
+    with open(ruta, "r+") as archivo:
+        tasas = json.load(archivo)
+        for moneda in tasas["USD"]:
+            nueva_tasa = tasas["USD"][moneda] * (0.98 + (0.04 * random.random()))
+            tasas["USD"][moneda] = round(nueva_tasa, 2)
+        tasas["actualizacion"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        archivo.seek(0)
+        json.dump(tasas, archivo, indent=2)
+        archivo.truncate()
 
 # Ejemplo de uso
 if __name__ == "__main__":
-<<<<<<< HEAD
-   tasas = cargar_tasas("../data/tasas.json")
-   precio_usd = 100.00
-   precio_eur = convertir(precio_usd, "EUR", tasas)
-   registrar_transaccion("Laptop", precio_eur, "EUR", "../logs/historial.txt")
-=======
-   tasas = cargar_tasas("data/tasas.json")
-   precio_usd = 100.00
-   precio_eur = convertir(precio_usd, "EUR", tasas)
-   registrar_transaccion("Laptop", precio_eur, "EUR", "logs/historial.txt")
->>>>>>> 3b0a421 (restructurar archivos)
+    # Actualizar las tasas
+    actualizar_tasas("data/tasas.json")
+    tasas = cargar_tasas("data/tasas.json")
+    precio_usd = 100.00
+    precio_eur = convertir(precio_usd, "EUR", tasas)
+    registrar_transaccion("Laptop", precio_eur, "EUR", "logs/historial.txt")
